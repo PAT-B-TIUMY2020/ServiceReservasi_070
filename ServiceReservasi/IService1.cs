e0c49699e22eea6777a7c91a3690bb67b520cb78
syafiraJafar <56532146+syafiraJafar@users.noreply.github.com>
2020-11-27 16:29:56 +0700

    add dataBase

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -17,7 +17,62 @@ namespace ServiceReservasi
         [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
+        //proses input data
+        [OperationContract]
+        string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi);
+
+        [OperationContract]
+        string editPemesanan(string IDPemesanan, string NamaCustomer);
+
+        [OperationContract]
+        string deletePemesanan(string IDPemesanan);
+
+        [OperationContract]
+        List<CekLokasi> ReviewLokasi(); // menamplkan data data yang ada di database
+
+        [OperationContract]
+        List<DetailLokasi> DetailLokasi();
+
+        [OperationContract]
+        List<Pemesanan> Pemesanan();
+
         // TODO: Add your service operations here
+    }
+    [DataContract]
+    public class CekLokasi //daftar lokasi
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variable dari public class
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeksripsiSingkat { get; set; }
+    }
+    [DataContract]
+    public class DetailLokasi
+    {
+        [DataMember]
+        public string IDLokasi { get; set; }
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeksripsiFull { get; set; }
+        [DataMember]
+        public int Kuota { get; set; }
+    }
+    [DataContract]
+    public class Pemesanan
+    {
+        [DataMember]
+        public string IDPemesanan { get; set; }
+        [DataMember]
+        public string NamaCustomer { get; set; }
+        [DataMember]
+        public string NoTelepon { get; set; }
+        [DataMember]
+        public int JumlahPemesanan { get; set; }
+        [DataMember]
+        public string IDLokasi { get; set; }
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
