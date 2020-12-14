c893f26b26dc29f4993297e261eeea29ca40ac0e
syafiraJafar <56532146+syafiraJafar@users.noreply.github.com>
2020-12-14 22:49:01 +0700

    Membuat Data Register

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -24,6 +24,17 @@ namespace ServiceReservasi
         string deletePemesanan(string IDPemesanan);
 
         [OperationContract]
+        string Login(string username, string password);
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+        [OperationContract]
+        string DeleteRegister(string username);
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
+        [OperationContract]
         List<CekLokasi> ReviewLokasi(); // menamplkan data data yang ada di database
 
         [OperationContract]
@@ -69,6 +80,19 @@ namespace ServiceReservasi
         public int JumlahPemesanan { get; set; }
         [DataMember]
         public string Lokasi { get; set; }
+    }
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
+
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
